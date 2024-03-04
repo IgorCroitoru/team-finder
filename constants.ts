5266319e9d3515e5ad045897f0f00db50ac976d9
IgorCroitoru <croitoru-2013@mail.ru>
2024-03-05 00:44:55 +0200

    same-site

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-
+import { CookieOptions } from "express";
 // DECLARED IN SECONDS FOR USING IN JWT
 export const ACCESS_TOKEN_EXPIRATION = 30*60; // (30m)
 export const INVITATION_LINK_EXPIRATION = 60*60*24*7; // 7D
@@ -6,12 +6,16 @@ export const REFRESH_TOKEN_EXPIRATION = 60*60*24*15 //15D
 
 
 
-export const COOKIE_SETTINGS = {
-    REFRESH_TOKEN: {
+export const COOKIE_SETTINGS: {
+  REFRESH_TOKEN: CookieOptions;
+} = {
+  REFRESH_TOKEN: {
       httpOnly: true,
-      maxAge: REFRESH_TOKEN_EXPIRATION * 1000, //DECLARED IN MILLISECONDS (15d)
-    },
-  };
+      maxAge: REFRESH_TOKEN_EXPIRATION * 1000, // Convert seconds to milliseconds
+      sameSite: 'none',
+      secure: true, // Remember, SameSite=None requires Secure=true, especially important for production
+  },
+};
   
   //In seconds
   

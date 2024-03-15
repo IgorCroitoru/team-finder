f7290c6504d789509f7ede5be97ccd47b627d9c4
IgorCroitoru <croitoru-2013@mail.ru>
2024-03-15 22:15:11 +0200

    delete cors

--- main.ts
+++ main.ts
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   }
 
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
   next();

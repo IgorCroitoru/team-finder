9f8f5efbbafbf7e393256484a68d3627672ba7b7
IgorCroitoru <croitoru-2013@mail.ru>
2024-03-18 19:10:39 +0200

    origin

--- main.ts
+++ main.ts
@@ -23,9 +23,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const apiRoutes = express.Router();
-
+// origin: 'https://atc-2024-codebros-fe-linux-web-app.azurewebsites.net'
 const corsOptions = {
-  origin: 'https://atc-2024-codebros-fe-linux-web-app.azurewebsites.net',
+  origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204

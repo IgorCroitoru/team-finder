e40cdb857173b36aee8fa034310b0704e3a0692e
IgorCroitoru <croitoru-2013@mail.ru>
2024-03-20 10:53:31 +0200

    cors

--- main.ts
+++ main.ts
@@ -26,35 +26,35 @@ const PORT = process.env.PORT || 3000;
 const apiRoutes = express.Router();
 // origin: 'https://atc-2024-codebros-fe-linux-web-app.azurewebsites.net'
 const corsOptions = {
-  origin: '*',
+  origin: 'https://atc-2024-codebros-fe-linux-web-app.azurewebsites.net',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204
 };
-app.use((req, res, next) => {
-  // Get the origin of the request
-  const origin = req.headers.origin;
+// app.use((req, res, next) => {
+//   // Get the origin of the request
+//   const origin = req.headers.origin;
 
-  if (origin) {
-    // Set the Access-Control-Allow-Origin header to the requesting origin
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    // Allow credentials
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-  }
+//   if (origin) {
+//     // Set the Access-Control-Allow-Origin header to the requesting origin
+//     res.setHeader('Access-Control-Allow-Origin', origin);
+//     // Allow credentials
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   }
 
-  // Set other CORS headers as needed
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//   // Set other CORS headers as needed
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
-  // If it's a preflight (OPTIONS) request, respond with 204 'No Content'
-  if (req.method === 'OPTIONS') {
-    return res.status(204).end();
-  }
+//   // If it's a preflight (OPTIONS) request, respond with 204 'No Content'
+//   if (req.method === 'OPTIONS') {
+//     return res.status(204).end();
+//   }
 
-  next();
-});
+//   next();
+// });
 
-//app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 //app.use(deserialize);
